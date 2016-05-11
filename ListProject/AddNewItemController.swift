bd89b94e7a8c158ff7e0b4b4325acf5fb5f9a19f
Michael Arbogast <marboga@gmail.com>
2016-05-11 10:07:42 -0700

    Added controller

---
+++ ListProject/AddNewItemController.swift
@@ -0,0 +1,13 @@
+//
+//  AddNewItemController.swift
+//  ListProject
+//
+//  Created by Michael Arbogast on 5/11/16.
+//  Copyright © 2016 Vu Dinh. All rights reserved.
+//
+
+import UIKit
+
+class AddNewItemController: UITableViewController {
+    
+}
